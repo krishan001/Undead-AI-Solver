77bd99cb8c97fbc6dbb27bab545009d6791d5b8f
Krishan Patel <sc17kdp@leeds.ac.uk>
2019-12-08 16:36:07 +0000

    renamed trace_path2 to trace_path

--- tracePath.py
+++ tracePath.py
@@ -1,4 +1,4 @@
-def trace_path2(matrix, x, y, d, numRows, numCols):
+def trace_path(matrix, x, y, d, numRows, numCols):
     path = []
     while(x < numCols and y < numRows and x >=0 and y>=0): #in the bounds of the matrix
         #check square is not a mirror
@@ -16,21 +16,21 @@ def trace_path2(matrix, x, y, d, numRows, numCols):
         elif (matrix[y][x] == "/" and d == 0) or (matrix[y][x] == "\\" and d == 2):
             d = 1
             x+=1
-            return path.extend(trace_path2(matrix, x, y, d, numRows, numCols))
+            return path.extend(trace_path(matrix, x, y, d, numRows, numCols))
 
         elif (matrix[y][x] == "/" and d == 1) or (matrix[y][x] == "\\" and d == 3):
             d = 0
             y-=1
-            return path.extend(trace_path2(matrix, x, y, d, numRows, numCols))
+            return path.extend(trace_path(matrix, x, y, d, numRows, numCols))
 
         elif (matrix[y][x] == "/" and d == 2) or (matrix[y][x] == "\\" and d == 0):
             d = 3
             x-=1
-            return path.extend(trace_path2(matrix, x, y, d, numRows, numCols))
+            return path.extend(trace_path(matrix, x, y, d, numRows, numCols))
 
         elif (matrix[y][x] == "/" and d == 3) or (matrix[y][x] == "\\" and d == 1):
             d = 2
             y+=1
-            return path.extend(trace_path2(matrix, x, y, d, numRows, numCols))
+            return path.extend(trace_path(matrix, x, y, d, numRows, numCols))
 
     return path
