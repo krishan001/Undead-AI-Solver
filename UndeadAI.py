87806a6ef91ce2df7b965710ec11c0d1bea3599b
Krishan Patel <sc17kdp@leeds.ac.uk>
2020-03-27 14:18:27 +0000

    tested on 5x5 easy

--- UndeadAI.py
+++ UndeadAI.py
@@ -29,9 +29,9 @@ numGhosts, numVampires,numZombies = 0,0,0
 def main():
     global numGhosts, numVampires,numZombies
     # define the dimentions of the board
-    dim = 4
+    dim = 5
     #read the board from a file
-    matrix, vis, numGhosts, numVampires,numZombies = readBoard("4x4.txt", 1, dim)
+    matrix, vis, numGhosts, numVampires,numZombies = readBoard("board.txt", 1, dim)
     # Print original board
     printBoard(matrix,vis, dim, numGhosts, numVampires,numZombies)
     # Fill in the paths that have 0 visible
